d3921c807cc728232d9c15a26bf1c1c4d618fe7c
Jihye Eom <37167493+wisdeom@users.noreply.github.com>
2018-11-09 01:10:51 +0900

    Create appcenter-post-clone.sh

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# install correct node version
+set -ex
+brew uninstall node@6
+NODE_VERSION="8.9.4"
+curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
+sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+
+# install apollo
+npm install -g apollo
+
+# setup cocoa pods keys
+gem install cocoapods-keys
